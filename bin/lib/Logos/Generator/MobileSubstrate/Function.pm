d893aec3b62b13726164238b78551313538324d9
ren7995 <ren7995@users.noreply.github.com>
2021-06-09 19:03:31 -0400

    basic libhooker api support (no function batching yet)

--- bin/lib/Logos/Generator/MobileSubstrate/Function.pm
+++ bin/lib/Logos/Generator/MobileSubstrate/Function.pm
@@ -6,12 +6,18 @@ sub initializers {
 	my $self = shift;
 	my $function = shift;
 
-	my $return = "";
-	$return .= "void * ".$self->variable($function)." = ".$self->_initExpression($function).";";
+	my $return = "{typedef void (*LOGOS_hookfuncs_ptr_t)(const struct LHFunctionHook *, int);LOGOS_hookfuncs_ptr_t LOGOS_C_HOOK_FUNC = (LOGOS_hookfuncs_ptr_t)dlsym(((void *)0), \"LHHookFunctions\");";
+	$return .= "if(LOGOS_C_HOOK_FUNC == NULL) {void * ".$self->variable($function)." = ".$self->_initExpression($function).";";
 	$return .= " MSHookFunction((void *)".$self->variable($function);
 	$return .= ", (void *)&".$self->newFunctionName($function);
 	$return .= ", (void **)&".$self->originalFunctionName($function);
-	$return .= ");";
+	$return .= ");}else{";
+	$return .= "void * ".$self->variable($function)." = ".$self->_initExpression($function).";";
+	## TODO: Add the LHFunctionHook struct to an array and then call LHHookFunctions only once
+	$return .= "LHFunctionHook hook;hook.function = (void *)".$self->variable($function);
+	$return .= ";hook.replacement = (void *)&".$self->newFunctionName($function);
+	$return .= ";hook.oldptr = (void **)&".$self->originalFunctionName($function);
+	$return .= ";const struct LHFunctionHook *hooks = {&hook};LOGOS_C_HOOK_FUNC(hooks, 1);}}";
 
 	return $return;
 }
