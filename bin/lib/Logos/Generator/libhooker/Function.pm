aab48d106d67b5e8d5141f035f44ab571457564c
uroboro <uroboro.845@gmail.com>
2022-08-30 01:01:11 +0200

    [libhooker/%hookf] Support LHHookFunctions by subclassing Group
    Function initializers returns a struct of 3 pointers, Group initializers joins them as an array to pass to LHHookFunctions

--- bin/lib/Logos/Generator/libhooker/Function.pm
+++ bin/lib/Logos/Generator/libhooker/Function.pm
@@ -3,7 +3,16 @@ use strict;
 use parent qw(Logos::Generator::Base::Function);
 
 sub initializers {
-	die "Unsupported. Use LHHookFunctions directly until theos is updated."
+	my $self = shift;
+	my $function = shift;
+
+	my $return = "";
+	$return .= "{".$self->_initExpression($function);
+	$return .= ", (void *)&".$self->newFunctionName($function);
+	$return .= ", (void **)&".$self->originalFunctionName($function);
+	$return .= "}";
+
+	return $return;
 }
 
 1;
