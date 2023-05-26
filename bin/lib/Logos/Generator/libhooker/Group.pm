5c98ae6402258cfaaa30c96d2c593bdc4bcc9628
Lightmann <lightmanns11@gmail.com>
2023-05-26 12:28:12 -0400

    Properly fallback for LHHookFunctions if no structs

--- bin/lib/Logos/Generator/libhooker/Group.pm
+++ bin/lib/Logos/Generator/libhooker/Group.pm
@@ -10,6 +10,7 @@ sub initializers {
 		$return .= Logos::Generator::for($_)->initializers if $_->initRequired;
 	}
 	my @structs = map { Logos::Generator::for($_)->initializers } @{$group->functions};
+	push(@structs, "nil") if(!@structs);
 	my $functionCount = @{$group->functions};
 	$return .= "LHHookFunctions(".join(",", @structs).", ".$functionCount.");";
 	$return .= "}";
