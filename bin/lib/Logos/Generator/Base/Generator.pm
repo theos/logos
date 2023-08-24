e29af84c823154f8f95afb4e276f4c550dfe00b3
Lightmann <lightmanns11@gmail.com>
2023-08-24 09:21:54 -0400

    Simplify base generator's staticDeclarations

--- bin/lib/Logos/Generator/Base/Generator.pm
+++ bin/lib/Logos/Generator/Base/Generator.pm
@@ -15,18 +15,11 @@ sub preamble {
 sub staticDeclarations {
 	my $self = shift;
 	return <<END;
-#if defined(__clang__)
-#if __has_feature(objc_arc)
+#if defined(__clang__) && __has_feature(objc_arc)
 #define _LOGOS_SELF_TYPE_NORMAL __unsafe_unretained
 #define _LOGOS_SELF_TYPE_INIT __attribute__((ns_consumed))
 #define _LOGOS_SELF_CONST const
 #define _LOGOS_RETURN_RETAINED __attribute__((ns_returns_retained))
-#else
-#define _LOGOS_SELF_TYPE_NORMAL
-#define _LOGOS_SELF_TYPE_INIT
-#define _LOGOS_SELF_CONST
-#define _LOGOS_RETURN_RETAINED
-#endif
 #else
 #define _LOGOS_SELF_TYPE_NORMAL
 #define _LOGOS_SELF_TYPE_INIT
